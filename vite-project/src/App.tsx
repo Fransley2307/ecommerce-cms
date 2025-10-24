06e277fec8f39cb62318880995ff6fe9d0cb6583
Fransley Brustolim Ferreira <fransley0@gmail.com>
2025-10-24 19:42:40 -0300

    Aula 22/10

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CategoryLayout } from "./cases/categories/components/category-layout"
 
 function App() {
-  function App() {
+
   return (
     <div className="wrapper">
+      <CategoryLayout></CategoryLayout>
     </div>
   )
 }
